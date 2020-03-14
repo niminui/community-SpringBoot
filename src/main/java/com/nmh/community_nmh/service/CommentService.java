64ac396fb603e3b1218ce9a0e7edccb259d58de8
niminui <1205608366@qq.com>
2020-03-14 23:48:26 +0800

    Updated reply function

--- src/main/java/com/nmh/community_nmh/service/CommentService.java
+++ src/main/java/com/nmh/community_nmh/service/CommentService.java
@@ -10,6 +10,7 @@ import com.nmh.community_nmh.model.Comment;
 import com.nmh.community_nmh.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author niminui
@@ -27,6 +28,7 @@ public class CommentService {
     @Autowired
     private QuestionExtMapper questionExtMapper;
 
+    @Transactional
     public void insert(Comment comment) {
         if (comment.getParentId() == null || comment.getParentId() == 0) {
             throw new CustomizeException(CustomizeErrorCode.TARGET_PARAM_NOT_FOUND);
