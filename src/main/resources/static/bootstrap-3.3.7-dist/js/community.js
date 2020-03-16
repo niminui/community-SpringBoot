8626e216bea425a55f730ed929bba809162d0afe
niminui <1205608366@qq.com>
2020-03-16 11:14:49 +0800

    更新二级回复展示

--- src/main/resources/static/bootstrap-3.3.7-dist/js/community.js
+++ src/main/resources/static/bootstrap-3.3.7-dist/js/community.js
@@ -72,12 +72,36 @@ function collapseComments(e) {
         } else {
             $.getJSON("/comment/" + id, function (data) {
                 $.each(data.data.reverse(), function (index, comment) {
-                    console.log(comment);
-                    var c = $("<div/>", {
+                    var mediaLeftElement = $("<div/>", {
+                        "class": "media-left"
+                    }).append($("<img/>", {
+                        "class": "media-object img-rounded",
+                        "src": comment.user.avatarUrl
+                    }));
+
+                    var mediaBodyElement = $("<div/>", {
+                        "class": "media-body"
+                    }).append($("<h5/>", {
+                        "class": "media-heading",
+                        "html": comment.user.name
+                    })).append($("<div/>", {
+                        "html": comment.content
+                    })).append($("<div/>", {
+                        "class":"menu"
+                    }).append($("<span/>", {
+                        "class":"pull-right",
+                        "html":comment.gmtCreate
+                    })));
+
+                    var mediaElement = $("<div/>", {
+                        "class": "media"
+                    }).append(mediaLeftElement).append(mediaBodyElement);
+
+                    var commentElement = $("<div/>", {
                         "class": "col-lg-12 col-md-12 col-sm-12 col-xs-12 comments",
-                        html: comment.content
-                    });
-                    subCommentContainer.prepend(c);
+                    }).append(mediaElement);
+
+                    subCommentContainer.prepend(commentElement);
                 });
                 //展开二级评论
                 comments.addClass("in");
